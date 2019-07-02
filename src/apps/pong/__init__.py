eaca57e0ebde1665d01763905d532d7ba3c9ea56
Richie Bendall <richiebendall@gmail.com>
2019-07-02 17:04:22 +1200

    chore: Add linting
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

---
+++ src/apps/pong/__init__.py
@@ -0,0 +1,16 @@
+"""Pong game."""
+import turtle
+
+WN = turtle.Screen()
+WN.title("Megapong")
+WN.bgcolour("black")
+WN.setup(width=800, height=600)
+WN.tracer(0)
+
+PADDLE_A = turtle.Turtle()
+PADDLE_A.speed(0)
+PADDLE_A.shape("square")
+PADDLE_A.color("white")
+
+while True:
+    WN.update()
