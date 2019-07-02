28e7094c6931bc1556ccb187979390fc407e0388
lunala9 <37818419+lunala9@users.noreply.github.com>
2019-07-03 10:53:06 +1200

    Update __init__.py

--- src/apps/pong/__init__.py
+++ src/apps/pong/__init__.py
@@ -11,6 +11,17 @@ PADDLE_A = turtle.Turtle()
 PADDLE_A.speed(0)
 PADDLE_A.shape("square")
 PADDLE_A.color("white")
+PADDLE_A.shapesize(stretch_wid=5, stretch_len=1)
+PADDLE_A.penup()
+PADDLE_A.goto(-350, 0)
+
+PADDLE_B = turtle.Turtle()
+PADDLE_B.speed(0)
+PADDLE_B.shape("square")
+PADDLE_B.color("white")
+PADDLE_B.shapesize(stretch_wid=5, stretch_len=1)
+PADDLE_B.penup()
+PADDLE_B.goto(350, 0)
 
 while True:
     WN.update()
