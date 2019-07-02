87e6eefa083caef19e0440ba07ad1e52a1abee93
lunala9 <37818419+lunala9@users.noreply.github.com>
2019-07-02 12:56:04 +1200

    Update index.py

--- src/apps/pong/index.py
+++ src/apps/pong/index.py
@@ -3,3 +3,14 @@ import turtle
 wn = turtle.Screen()
 wn.title("Megapong")
 wn.bgcolour("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+paddle_a = turtle.turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.colour("white")
+
+while True:
+  wn.update()
+  
